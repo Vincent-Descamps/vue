5e9d6e4e190cd94a94d80d814d9a6fa9aed8d332
Vince <vincedescamps@gmail.com>
2024-10-10 16:07:58 +0200

    vue_express

---
+++ vue-express-webapp/server/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import path from "path";
+import homePageRouter from "./homePageRouter.js";
+import assetsRouter from "./assetsRouter.js";
+
+const port = process.env.PORT || 3000;
+const publicPath = path.join(path.resolve(), "public");
+
+const app = express();
+
+app.get("/api/v1/hello", (_req, res) => {
+  res.json({ message: "Hello, world!" });
+});
+
+app.use(express.static(publicPath));
+app.use("/src", assetsRouter);
+app.use(homePageRouter);
+
+app.listen(port, () => {
+  console.log("Server listening on port", port);
+});
--- vue-express-webapp/src/components/HelloWorld.vue
+++ vue-express-webapp/src/components/HelloWorld.vue
@@ -6,10 +6,19 @@ defineProps({
 })
 
 const count = ref(0)
+
+const serverHello = ref({})
+
+fetch('/api/v1/hello')
+  .then((response) => response.json())
+  .then(({ message }) => {
+    serverHello.value = message
+  })
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
+  <h2>{{ serverHello }}</h2>
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
